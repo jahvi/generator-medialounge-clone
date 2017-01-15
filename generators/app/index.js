0bd5f405777da6ee254c6bdebd8255d2430941dc
Javier Villanueva <javiervd@gmail.com>
2017-01-15 18:37:39 +0000

    Add initial support to generate local.xml file

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,9 +4,12 @@ var Generator = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var Spinner = require('cli-spinner').Spinner;
+var execFile = require('child_process').execFile;
+var fs = require('fs-extra');
 
 module.exports = Generator.extend({
   prompting: function () {
+
     // Have Yeoman greet the user.
     this.log(yosay(
       'Welcome to the wicked ' + chalk.red('Media Lounge Clone') + ' generator!'
@@ -17,6 +20,26 @@ module.exports = Generator.extend({
       name: 'repo',
       message: `Project name? https://bitbucket.org/medialounge_repo/${chalk.red('name')}`,
       default: this.appname
+    }, {
+      type: 'input',
+      name: 'dbHost',
+      message: 'Database host?',
+      default: '192.168.56.101'
+    }, {
+      type: 'input',
+      name: 'dbUsername',
+      message: 'Database username?',
+      default: 'root'
+    }, {
+      type: 'password',
+      name: 'dbPassword',
+      message: 'Database password?',
+      default: 'root'
+    }, {
+      type: 'input',
+      name: 'dbName',
+      message: 'Database name?',
+      default: `mg_${this.appname}`
     }];
 
     return this.prompt(prompts).then(props => {
@@ -26,11 +49,14 @@ module.exports = Generator.extend({
 
   writing: {
     cloneRepo() {
-      var execFile = require('child_process').execFile;
+      var done = this.async();
       var spinner = new Spinner('Cloning git repo...');
 
       spinner.setSpinnerString(18);
       spinner.start();
+
+      // Make sure current directory is empty
+      fs.emptyDirSync(this.destinationRoot());
 
       execFile(
         'git',
@@ -44,7 +70,40 @@ module.exports = Generator.extend({
             this.log.error(error);
           }
 
-          spinner.stop();
+          spinner.stop(true);
+          this.log.ok('Cloning git repo...');
+
+          done();
+        }
+      );
+    },
+    createLocalXml() {
+      var localXml = this.destinationPath('app/etc/local.xml');
+      var spinner = new Spinner('Generating local.xml file...');
+
+      spinner.setSpinnerString(18);
+      spinner.start();
+
+      fs.removeSync(localXml);
+
+      execFile(
+        'n98-magerun',
+        [
+          'local-config:generate',
+          this.props.dbHost,
+          this.props.dbUsername,
+          this.props.dbPassword,
+          this.props.dbName,
+          'files',
+          'admin'
+        ],
+        error => {
+          if (error) {
+            this.log.error(error);
+          }
+
+          spinner.stop(true);
+          this.log.ok('Generating local.xml file...');
         }
       );
     }
